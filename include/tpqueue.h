726b4f9454fd60835aa27affca72aee09feff2bd
daria <75723901+dariabo4kareva@users.noreply.github.com>
2021-05-12 19:53:56 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -3,14 +3,69 @@
 #define INCLUDE_TPQUEUE_H_
 #include <cassert>
 
-template<typename T>
+template <typename T>
 class TPQueue {
-  // Сюда помещается описание структуры "Очередь с приоритетами"
+ private:
+  struct ITEM {
+    T data;
+    ITEM* previous;
+    ITEM* next;
+  };
+  ITEM* strt;
+  ITEM* fnsh;
+  TPQueue::ITEM* create(const T& data, ITEM* previous) {
+    ITEM* item = new ITEM;
+    item->data = data;
+    item->next = nullptr;
+    item->previous = previous;
+    return item;
+  }
+
+ public:
+  TPQueue() : strt(nullptr), tail(nullptr) {}
+  ~TPQueue() {
+    while (strt) pop();
+  }
+  void push(const T& data) {
+    if (fnsh && strt) {
+      ITEM* crrnt = fnsh;
+      while (crrnt && data.prior > (crrnt->data).prior) {
+        crrnt = crrnt->previous;
+      }
+      if (crrnt) {
+        ITEM* tmp = crrnt->next;
+        crrnt->next = create(data, crrnt);
+        crrnt = crrnt->next;
+        crrnt->next = tmp;
+        if (tmp) {
+          tmp->previous = crrnt;
+        } else {
+          fnsh = crrnt;
+        }
+      } else {
+        crrnt = create(data, nullptr);
+        crrnt->next = strt;
+        strt->previous = crrnt;
+        strt = crrnt;
+      }
+    } else {
+      strt = create(data, nullptr);
+      fnsh = strt;
+    }
+  }
+  T pop() {
+    assert(strt);
+    ITEM* tmp = strt->next;
+    T data = strt->data;
+    if (tmp) tmp->previous = nullptr;
+    delete strt;
+    strt = tmp;
+    return data;
+  }
 };
 
 struct SYM {
   char ch;
-  int  prior;
+  int prior;
 };
-
-#endif // INCLUDE_TPQUEUE_H_
+#endif  // INCLUDE_TPQUEUE_H_
